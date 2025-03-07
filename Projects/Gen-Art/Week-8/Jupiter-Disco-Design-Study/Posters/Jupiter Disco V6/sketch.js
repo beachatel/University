27b950cd091a4dcbc702bdcf9568f4564d5b0c05
Bea Chatel <bchatelbell@gmail.com>
2025-03-07 13:54:25 +0000

    Jupiter Disco page styling updates

--- Projects/Gen-Art/Week-8/Jupiter-Disco-Design-Study/Posters/Jupiter Disco V6/sketch.js
+++ Projects/Gen-Art/Week-8/Jupiter-Disco-Design-Study/Posters/Jupiter Disco V6/sketch.js
@@ -1,74 +1,62 @@
+let sketch6 = function(p){
+
+
+
 let angle = 0.1;
 let amplitude = 100;
 let gridSize = 300;
 let gridSize1 = 200;
 let font;
 
-function preload(){
+p.preload = function(){
   font = loadFont("Fonts/Magistral.ttf")
 }
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-   background("#0b0b0b");
+p.setup = function() {
+  p.createCanvas(p.windowWidth / 2, p.windowHeight);
+   p.background("#0b0b0b");
 }
 
-function draw() {
-  fill(30, 4, 158)
+p.draw = function() {
+  p.fill(30, 4, 158)
   //  for (let x = 100; x < width - 100; x+= gridSize1){
   //   for (let y = 50; y < height - 200; y+= gridSize1){
   //     ellipse(x,y,10,10);
   //   }
   // }
-  push();
-  translate(width/2,height/2.5);
-  scale(2);
-
-    fill(30, 4, 158);
- 
-
- 
-
-  for (let x = 0; x < width; x += gridSize * 5){
-    for (let y = 0; y < height; y+= gridSize * 5){
+  p.push();
+ p.translate(p.width/2,p.height/2.5);
+  p.scale(2);
 
 
-
-
-  let y1  = map(sin(angle),-1,2,-amplitude,amplitude);
-    let x1  = map(cos(angle),-1,2,-amplitude,amplitude);
- 
-   
-      // rotate(angle / 1.5);
-
-  fill(30, 4, 158);
   
-  stroke(118, 191, 241);
- 
-   ellipse(x -= angle ,y,x1 * 5,y1 *-2);
  
 
 
 
-    }
-  }
-pop();
 
-
-
-  fill(30, 4, 158);
-  noStroke();
-  rectMode(CENTER);
-    rect(0,height - 90,width  *2, height / 5);
-    fill(118, 191, 241);
-    rect(0,height,width  *2, height / 6);
-  fill(255);
-  textFont(font);
-  textSize(90)
+    
   
-    text("JUPITER DISCO",width / 30,height / 1.12)
-    textSize(40);
-    fill(30, 4, 158);
-        text("Animated Poster Concept Series V6", width / 28,height / 1.02)
+  p.pop();
+
+
+
+  p.fill(30, 4, 158);
+  p.noStroke();
+  p.rectMode(p.CENTER);
+  p.rect(0,p.height - 90, p.width  *2, p.height / 5);
+  p.fill(118, 191, 241);
+  p.rect(0,p.height, p.width  *2, p.height / 6);
+  p.fill(255);
+  p.textFont(font);
+  p.textSize(90)
+  
+  p.text("JUPITER DISCO",p.width / 30,p.height / 1.12)
+  p.textSize(40);
+  p.fill(30, 4, 158);
+  p.text("Animated Poster Concept Series V6", p.width / 28,p.height / 1.02)
 
    angle += 0.01
+
 }
+} 
+new p5(sketch6,'sketch6container');
